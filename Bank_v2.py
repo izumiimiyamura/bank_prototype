b625ab23edb0cbe3d1f325fa4c09fb0dc7011dfc
iZuMii <vipulgamerx@gmail.com>
2026-04-18 21:20:06 +0530

    Add amount validation with withdraw function

--- Bank_v2.py
+++ Bank_v2.py
@@ -1,4 +1,29 @@
-class BankAccount:
+class Transactions:
+    # Validates the amount given by the user.
+    def validate_amount(self, amount):
+        # Type checking the amount.
+        if not isinstance(amount, (int,float)):
+            try:
+                return float(amount)
+            except ValueError:
+                return "Invalid Amount"
+
+        #Checking if the amount is negative.
+        if amount < 0 :
+            return "The Amount cannot be less than zero"
+        
+        # If nothing wrong return amount.
+        return amount
+
+    def withdraw(self):
+        amount = self.validate_amount(float(input("Amount to withdraw: ")))
+        if self.balance > amount:
+            self.balance -= amount
+            print(f"Remaining Balance: {self.balance}")
+        return "Insufficient Balance"
+
+
+class BankAccount(Transactions):
     def __init__(self, name, balance):
         self.name = name
         self.balance = balance
@@ -11,5 +36,13 @@ class BankAccount:
     @classmethod
     def create(cls):
         name = input("Name: ")
-        balance = float(input("Balance: "))
+        try:
+            balance = float(input("Balance: "))
+        except ValueError:
+            return "Not a Valid Balance."
         return cls(name, balance)
+
+
+
+bank_account_1 = BankAccount.create()
+bank_account_1.withdraw()
